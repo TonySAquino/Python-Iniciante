9788d4c78f54a496cbc61741296cdbd4d36acd6b
Tony <tonysilovi@gmail.com>
2022-09-06 22:47:31 -0300

    ultimo commit envolvendo funções

---
+++ funções pt 3.py
@@ -0,0 +1,12 @@
+
+def opa(b):
+    a = f"opa {b}"
+    return a
+
+b = input('Qual o seu nome? ')
+opa(b)
+
+print("salve,", opa(b))
+
+#nesse código, acabamos de usar return, ou seja, quando a função for chamada, ela irá
+#retornar o parametro determinado
